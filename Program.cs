0554784014a27c4ff0a2cdc6a0cdc58f8d519392
reistie1 <josh.reist@gmail.com>
2022-01-07 19:28:58 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DatastructureAlgorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
